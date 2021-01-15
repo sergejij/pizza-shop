dd17d2612f1f6d7002996087ed1b267cadb50f6f
Ушаков Сергей Дмитриевич <SDUshakov@sberbank.ru>
2021-01-15 23:27:12 +0300

    categories are ready

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
-const Categories = ({ items, onClick }) => {
+const Categories = ({ items }) => {
+    const [activeItem, setActiveItem] = React.useState(null);
+
     return (
         <div className="categories">
             <ul>
-                <li className='active'>Все</li>
-                {items.map((item, index) => (
-                    <li onClick={() => onClick(item)} key={`${item}_${index}`}>{item}</li>
+                <li
+                    onClick={() => setActiveItem(null)}
+                    className={activeItem === null ? 'active' : ''}
+                >
+                    Все
+                </li>
+                {items && items.map((item, index) => (
+                    <li
+                        onClick={() => setActiveItem(index)}
+                        key={`${item}_${index}`}
+                        className={activeItem === index ? 'active' : ''}
+                    >
+                        {item}
+                    </li>
                 ))}
             </ul>
         </div>
