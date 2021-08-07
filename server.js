8074d3889d932c04f9167a5d16d7648b70ff3554
sergejij <ushakoff.sergej2010@yandex.ru>
2021-08-07 20:32:42 +0300

    deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 
 const server = jsonServer.create();
-const router = jsonServer.router('./src/pizzas.json');
+const router = jsonServer.router('./public/pizzas.json');
 const middlewares = jsonServer.defaults({
   static: './build',
 });
